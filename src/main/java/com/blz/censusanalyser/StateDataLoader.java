933a1a1c42a607e66efd87be83c8f7347b4a32e0
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-12 17:11:06 +0530

    Refactor: To use the List in the place of iterator to count the records

--- src/main/java/com/blz/censusanalyser/StateDataLoader.java
+++ src/main/java/com/blz/censusanalyser/StateDataLoader.java
@@ -4,48 +4,45 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.List;
 
 public class StateDataLoader {
 
 	public int loadCensusData(String path) throws CensusAnalyserException {
-		Iterator<CSVStateCensus> csvUserIterator = null;
+		List<CSVStateCensus> csvUserList;
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(path));
 			CsvBuilderInterface csvBuilder = new CSVBuilderFactory().getCSVBuilder();
-			csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class);
+			csvUserList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
 		} catch (IOException exception) {
 			throw new CensusAnalyserException(CensusAnalyserException.exceptionType.FILE_NOT_FOUND,
 					"file is not found");
 		} catch (CSVBuilderException exception) {
 			throw new CensusAnalyserException(exception.getMessage(), exception.type.name());
+		} catch (RuntimeException exception) {
+			throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_FILE,
+					"delimiter or header is improper");
 		}
-		return getCount(csvUserIterator);
+		return csvUserList.size();
 	}
 
 	public int loadStateCodeData(String path) throws CensusAnalyserException {
-		Iterator<IndianStateCode> csvUserIterator = null;
+		List<IndianStateCode> csvUserList;
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(path));
 			CsvBuilderInterface csvBuilderFactory = new CSVBuilderFactory().getCSVBuilder();
-			csvBuilderFactory.getCSVFileIterator(reader, IndianStateCode.class);
+			csvUserList = csvBuilderFactory.getCSVFileList(reader, IndianStateCode.class);
 
 		} catch (IOException exception) {
 			throw new CensusAnalyserException(CensusAnalyserException.exceptionType.FILE_NOT_FOUND,
 					"file is not found");
 		} catch (CSVBuilderException exception) {
 			throw new CensusAnalyserException(exception.getMessage(), exception.type.name());
+		} catch (RuntimeException exception) {
+			throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_FILE,
+					"delimiter or header is improper");
 		}
-		return getCount(csvUserIterator);
-	}
-
-	private <E> int getCount(Iterator<E> csvUserIterator) {
-		int count = 0;
-		while (csvUserIterator.hasNext()) {
-			count++;
-			csvUserIterator.next();
-		}
-		return count;
+		return csvUserList.size();
 	}
 
 }
