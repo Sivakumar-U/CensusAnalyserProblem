39b8144e5e74c38c94c4b80e858ff710127631fb
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-12 19:35:15 +0530

    Added: Sorting function to sort the states in alphabatical order

--- src/main/java/com/blz/censusanalyser/CensusAnalyserException.java
+++ src/main/java/com/blz/censusanalyser/CensusAnalyserException.java
@@ -5,7 +5,7 @@ public class CensusAnalyserException extends Exception {
 	private final exceptionType type;
 
 	public enum exceptionType {
-		FILE_NOT_FOUND, WRONG_FILE
+		FILE_NOT_FOUND, WRONG_FILE, NO_CENSUS_DATA
 	};
 
 	CensusAnalyserException(String message, String name) {
--- src/main/java/com/blz/censusanalyser/StateDataLoader.java
+++ src/main/java/com/blz/censusanalyser/StateDataLoader.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class StateDataLoader {
+	List<CSVStateCensus> csvUserList = null;
 
 	public int loadCensusData(String path) throws CensusAnalyserException {
-		List<CSVStateCensus> csvUserList;
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(path));
 			CsvBuilderInterface csvBuilder = CSVBuilderFactory.getCSVBuilder();
@@ -27,7 +30,6 @@ public class StateDataLoader {
 	}
 
 	public int loadStateCodeData(String path) throws CensusAnalyserException {
-		List<IndianStateCode> csvUserList;
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(path));
 			CsvBuilderInterface csvBuilder = CSVBuilderFactory.getCSVBuilder();
@@ -43,6 +45,28 @@ public class StateDataLoader {
 					"delimiter or header is improper");
 		}
 		return csvUserList.size();
+	}
+
+	public String getSortedCensusData() throws CensusAnalyserException {
+		if (csvUserList == null || csvUserList.size() == 0)
+			throw new CensusAnalyserException(CensusAnalyserException.exceptionType.NO_CENSUS_DATA, "no census data");
+		Comparator<CSVStateCensus> censusComparator = Comparator.comparing(census -> census.state);
+		this.sort(censusComparator);
+		String sortedStateCensusJson = new Gson().toJson(csvUserList);
+		return sortedStateCensusJson;
+	}
+
+	private void sort(Comparator<CSVStateCensus> censusComparator) {
+		for (int index1 = 0; index1 < csvUserList.size() - 1; index1++) {
+			for (int index2 = 0; index2 < csvUserList.size() - index1 - 1; index2++) {
+				CSVStateCensus census1 = csvUserList.get(index2);
+				CSVStateCensus census2 = csvUserList.get(index2 + 1);
+				if (censusComparator.compare(census1, census2) > 0) {
+					csvUserList.set(index2, census2);
+					csvUserList.set(index2 + 1, census1);
+				}
+			}
+		}
 	}
 
 }
