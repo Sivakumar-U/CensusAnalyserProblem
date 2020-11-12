4237ba23a4358e3a17888cf5e9be9207f278abc4
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-12 14:27:51 +0530

    Added: Test case to load the state code information from csv file

--- src/test/java/com/blz/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/blz/censusanalyser/StateCensusAnalyserTest.java
@@ -10,11 +10,20 @@ public class StateCensusAnalyserTest {
 	public final String WRONG_CSV_FILE_PATH = "C:\\Users\\Siva Reddy\\StateCensusD.csv";
 	public final String WRONG_TYPE_FILE_PATH = "C:\\Users\\Siva Reddy\\StateCensusData.txt";
 
+	public final String STATE_CODE_FILE_PATH = "C:\\Users\\Siva Reddy\\StateCode.csv";
+
 	private static StateCensusAnalyser censusAnalyser;
 
 	@BeforeClass
 	public static void createCensusAnalyserObj() {
 		censusAnalyser = new StateCensusAnalyser();
+	}
+
+	private static StateCodeHandler codeHandler;
+
+	@BeforeClass
+	public static void createCodeHandlerObj() {
+		codeHandler = new StateCodeHandler();
 	}
 
 	// TC-1.1
@@ -65,6 +74,17 @@ public class StateCensusAnalyserTest {
 			censusAnalyser.loadCensusData(CSV_FILE_PATH);
 		} catch (CensusAnalyserException exception) {
 			Assert.assertEquals("delimiter or header is improper", exception.getMessage());
+		}
+	}
+
+	// TC-2.1
+	@Test
+	public void givenStateCodeCsvFile_CheckNumberOfRecords_ShouldReturnNumber() {
+		try {
+			int numberOfStates = codeHandler.loadStateCodeData(STATE_CODE_FILE_PATH);
+			Assert.assertEquals(37, numberOfStates);
+		} catch (Exception exception) {
+			exception.getMessage();
 		}
 	}
 
